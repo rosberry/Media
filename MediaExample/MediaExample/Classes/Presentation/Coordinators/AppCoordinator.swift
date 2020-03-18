eed8a7c63b067f88d4d57a517ee73cd5ec2aab00
Artem Novichkov <artem.novichkov@rosberry.com>
2020-03-18 21:22:49 +0600

    Remove test logic

--- MediaExample/MediaExample/Classes/Presentation/Coordinators/AppCoordinator.swift
+++ MediaExample/MediaExample/Classes/Presentation/Coordinators/AppCoordinator.swift
@@ -11,14 +11,8 @@ final class AppCoordinator: BaseCoordinator<UIViewController> {
 
     init() {
         let navigationController = UINavigationController()
-//        let module = MediaLibraryModule(maxItemsCount: 1)
-//        let module = MediaLibraryItemsModule(maxItemsCount: 110)
-//        let module = MediaItemCollectionsModule()
-//        navigationController.viewControllers = [module.viewController]
         navigationController.viewControllers = [MainViewController()]
-//        module.viewController.navigationController?.navigationBar.isHidden = true
         super.init(rootViewController: navigationController)
-//        super.init(rootViewController: module.viewController)
     }
 
     func start(launchOptions: LaunchOptions?) {
