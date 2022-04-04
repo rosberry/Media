89d2c87da6afe0bd8c69de4ad4fe36cfc66770cd
Tublad <tublad92@gmail.com>
2022-04-04 13:43:22 +0600

    remove extra logc

--- MediaExample/MediaExample/Classes/Presentation/Modules/MainViewController/MainViewController.swift
+++ MediaExample/MediaExample/Classes/Presentation/Modules/MainViewController/MainViewController.swift
@@ -11,29 +11,9 @@ final class MainViewController: UIViewController {
 
     // MARK: - Subviews
 
-    private lazy var libraryButton: UIButton = {
-        let button = UIButton()
-        button.setTitle("Library", for: .normal)
-        button.setTitleColor(.black, for: .normal)
-        button.backgroundColor = .white
-        button.layer.cornerRadius = 8
-        button.addTarget(self, action: #selector(libraryButtonPressed), for: .touchUpInside)
-        return button
-    }()
-
-    private lazy var albumsButton: UIButton = {
-        let button = UIButton()
-        button.setTitle("Albums", for: .normal)
-        button.setTitleColor(.black, for: .normal)
-        button.backgroundColor = .white
-        button.layer.cornerRadius = 8
-        button.addTarget(self, action: #selector(albumsButtonPressed), for: .touchUpInside)
-        return button
-    }()
-
     private lazy var listButton: UIButton = {
         let button = UIButton()
-        button.setTitle("List", for: .normal)
+        button.setTitle("Gallery", for: .normal)
         button.setTitleColor(.black, for: .normal)
         button.backgroundColor = .white
         button.layer.cornerRadius = 8
@@ -42,7 +22,7 @@ final class MainViewController: UIViewController {
     }()
 
     private lazy var stackView: UIStackView = {
-        let view = UIStackView(arrangedSubviews: [libraryButton, albumsButton, listButton])
+        let view = UIStackView(arrangedSubviews: [listButton])
         view.axis = .vertical
         view.distribution = .fillEqually
         view.spacing = 8
@@ -67,51 +47,16 @@ final class MainViewController: UIViewController {
         }
     }
 
-    // MARK: - Private
-
-    @objc func libraryButtonPressed() {
-        start(with: .library)
-    }
-
-    @objc func albumsButtonPressed() {
-        start(with: .albums)
-    }
-
     @objc func listButtonPressed() {
-        start(with: .items)
+        start()
     }
 
-    private func start(with context: MediaCoordinator.Context) {
+    private func start() {
         guard let navigationController = navigationController else {
             return
         }
 
-        let cellAppearance = CellAppearance(contentViewCornerRadius: 5,
-                                           contentViewColor: .clear,
-                                           selectedColor: .green,
-                                           highlightedColor: .blue)
-        let sectionAppearance = SectionAppearance(minimumLineSpacing: 5,
-                                                 minimumInteritemSpacing: 5,
-                                                 insets: .init(top: 5, left: 5, bottom: 5, right: 5))
-
-        let library = CollectionViewAppearance(backgroundColor: .brown,
-                                               collectionViewBackgroundColor: .clear,
-                                               cellAppearance: cellAppearance,
-                                               sectionAppearance: sectionAppearance)
-
-        let albums = CollectionViewAppearance(backgroundColor: .yellow,
-                                              collectionViewBackgroundColor: .clear,
-                                              cellAppearance: cellAppearance,
-                                              sectionAppearance: sectionAppearance)
-
-        let list = CollectionViewAppearance(backgroundColor: .red,
-                                            collectionViewBackgroundColor: .yellow,
-                                            cellAppearance: cellAppearance,
-                                            sectionAppearance: sectionAppearance)
-
-        let mediaAppearance = MediaAppearance(library: library, albums: albums, list: list)
-        coordinator = .init(navigationViewController: navigationController, context: context, mediaAppearance: mediaAppearance)
-        coordinator?.numberOfItemsInRow = 3
-        coordinator?.start()
+        coordinator = .init(navigationViewController: navigationController)
+        coordinator?.start(bundleName: "Whats Up")
     }
 }
