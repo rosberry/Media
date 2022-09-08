9c0d2bb9b486198958146ae9b747216eb1a01df8
Tublad <tublad92@gmail.com>
2022-09-08 11:00:00 +0600

    add new resource

--- Media/Resources/UIColor.swift
+++ Media/Resources/UIColor.swift
@@ -6,6 +6,7 @@ import UIKit
 
 public extension UIColor {
     static let main1A: UIColor = .init(red: 0.0 / 255.0, green: 0.0 / 255.0, blue: 0.0 / 255.0, alpha: 0.3)
+    static let main2A: UIColor = .init(red: 255.0 / 255.0, green: 255.0 / 255.0, blue: 255.0 / 255.0, alpha: 1)
     static let main3A: UIColor = .init(red: 234.0 / 255.0, green: 234.0 / 255.0, blue: 234.0 / 255.0, alpha: 1)
 
     static let main1T: UIColor = .init(red: 0.0 / 255.0, green: 0.0 / 255.0, blue: 0.0 / 255.0, alpha: 1)
